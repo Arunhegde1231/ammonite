d75b831639a83f936106777725b9f4542bfbb1c8
Arunhegde1231 <arunhegde1231@outlook.com>
2024-03-18 23:15:41 +0530

    added system light and dark mode theme support.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:ammonite/discover.dart';
 import 'package:ammonite/home.dart';
 import 'package:ammonite/trending.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(MyApp());
 }
@@ -10,6 +11,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 209, 116, 225), brightness: Brightness.light,),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            fontSize: 72,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 125, 66, 227), brightness: Brightness.dark,),
+      ),
+      themeMode: ThemeMode.system,
       routes: {
         '/': (context) => Homescreen(),
         '/discover': (context) => DiscoverScreen(),
