ae312a485b0467627675d02f88c4f098308341da
Arun Hegde <arunhegde1231@outlook.com>
2024-03-19 20:34:09 +0530

    added placeholder notification screen

---
+++ lib/notifications.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const NotificationScreen());
+
+class NotificationScreen extends StatefulWidget {
+  const NotificationScreen({super.key});
+
+  @override
+  State<NotificationScreen> createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 209, 116, 225),
+          brightness: Brightness.light,
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            fontSize: 72,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 125, 66, 227),
+          brightness: Brightness.dark,
+        ),
+      ),
+      themeMode: ThemeMode.system,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Notifications')),
+        
+      ),
+    );
+  }
+}
