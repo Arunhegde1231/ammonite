dfd12175e8f1538dc62d95c7db410a8f1f50ddde
Arun Hegde <arunhegde@outlook.com>
2024-03-25 12:38:02 +0530

    added settings page

---
+++ lib/settings.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:system_theme/system_theme.dart';
+
+class settingsScreen extends StatefulWidget{
+  const settingsScreen({Key? key});
+  
+  @override
+  State<settingsScreen> createState()=> _settingsScreenState();
+}
+
+class _settingsScreenState extends State<settingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final accentcolor=SystemTheme.accentColor.accent;
+    int a=accentcolor.alpha;
+    int r=accentcolor.red;
+    int g=accentcolor.green;
+    int b=accentcolor.blue;
+    return MaterialApp(
+    theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromARGB(255, r, g, b),
+          brightness: Brightness.light,
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            fontSize: 72,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 125, 66, 227),
+          brightness: Brightness.dark,
+        ),
+      ),
+      themeMode: ThemeMode.system,
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+    ),
+    );
+  }
+}
