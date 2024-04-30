4e7ccb50561f8c88cc35ca4cd77d15177f58ca88
Arun Hegde <arunhegde@outlook.com>
2024-04-30 12:06:32 +0530

    added likes, dislikes, views counter and share, download button

--- lib/videoplayer.dart
+++ lib/videoplayer.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'package:video_player/video_player.dart';
 import 'package:chewie/chewie.dart';
@@ -96,16 +97,109 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
         child: GestureDetector(
           onTap: () {},
           child: _isInitialized
-              ? AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: Stack(
-                    alignment: FractionalOffset.bottomCenter +
-                        const FractionalOffset(-0.1, -0.1),
-                    children: [
-                      Chewie(controller: _chewieController),
-                    ],
-                  ),
-                )
+              ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                      _controller.value.isInitialized
+                          ? AspectRatio(
+                              aspectRatio: _controller.value.aspectRatio,
+                              child: Stack(
+                                alignment: FractionalOffset.bottomCenter +
+                                    const FractionalOffset(-0.1, -0.1),
+                                children: [
+                                  Chewie(controller: _chewieController),
+                                ],
+                              ),
+                            )
+                          : Container(),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(7.0),
+                            child: Text(
+                              name,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                  fontSize: 17, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Padding(
+                              padding: EdgeInsets.fromLTRB(0, 5, 7, 7),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  const Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 7.0)),
+                                  const Icon(Icons.thumb_up_outlined),
+                                  const SizedBox(width: 6),
+                                  Text('$likes'),
+                                  const SizedBox(width: 20),
+                                  const Icon(Icons.thumb_down_outlined),
+                                  const SizedBox(width: 6),
+                                  Text('$dislikes'),
+                                  const SizedBox(width: 20),
+                                  const Text('•'),
+                                  const SizedBox(width: 8),
+                                  Text('$views Views'),
+                                  IconButton(
+                                      padding: EdgeInsets.all(3),
+                                      onPressed: () {},
+                                      icon: Icon(Icons.share_outlined)),
+                                  IconButton(
+                                      padding: EdgeInsets.all(3),
+                                      onPressed: () {},
+                                      icon: Icon(Icons.download_outlined)),
+                                ],
+                              )),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(15, 5, 8, 8),
+                            child: TextButton(
+                              style: ButtonStyle(
+                                enableFeedback: true,
+                                padding: MaterialStateProperty.all<EdgeInsets>(
+                                  EdgeInsets.zero,
+                                ),
+                              ),
+                              onPressed: () {}, //TODO: add actions later
+                              child: Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      CircleAvatar(
+                                        backgroundImage: NetworkImage(
+                                          'https://tilvids.com$channelAvatar',
+                                        ),
+                                        radius: 20,
+                                      ),
+                                      const SizedBox(width: 8),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Expanded(
+                                          child: Text(
+                                            channelName,
+                                            style: const TextStyle(
+                                              fontSize: 13,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ])
               : const Center(child: CircularProgressIndicator()),
         ),
       ),
