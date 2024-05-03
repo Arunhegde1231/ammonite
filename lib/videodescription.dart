53b5d0971324dd7a2565297ed000e38ebad2047f
Arun Hegde <arunhegde@outlook.com>
2024-05-03 11:53:56 +0530

    moved the close button to the right

--- lib/videodescription.dart
+++ lib/videodescription.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 class VideoDescription extends StatelessWidget {
   final String description;
 
-  const VideoDescription(
-      {Key? key, required this.description,})
-      : super(key: key);
+  const VideoDescription({
+    Key? key,
+    required this.description,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,11 +29,25 @@ class VideoDescription extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          IconButton(
-            icon: Icon(Icons.arrow_upward_rounded),
-            onPressed: () {
-              Navigator.pop(context);
-            },
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  child: Row(
+                    children: [Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 5,5,5),
+                      child: Text('Description', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20,),),
+                    )],
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.close_outlined),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
           ),
           Expanded(
             child: SingleChildScrollView(
